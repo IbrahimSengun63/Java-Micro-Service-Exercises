00c003b2d8ddff2118f277d828e60005244e4e3c
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-07-26 15:12:44 +0300

    Product service entity and db connection added.

---
+++ productservice/src/main/java/com/ibrahim/productservice/entities/Product.java
@@ -0,0 +1,23 @@
+package com.ibrahim.productservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "stock")
+    private int stock;
+}
