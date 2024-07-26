dd26a01bd668087646f06f4ece12ec89dd49ca3a
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-07-26 16:28:07 +0300

    Product Service Map Struct implemantation added.

---
+++ productservice/src/main/java/com/ibrahim/productservice/mappers/ProductMapper.java
@@ -0,0 +1,32 @@
+package com.ibrahim.productservice.mappers;
+
+import com.ibrahim.productservice.dtos.requests.RequestAddProduct;
+import com.ibrahim.productservice.dtos.requests.RequestGetProductById;
+import com.ibrahim.productservice.dtos.requests.RequestGetProductByName;
+import com.ibrahim.productservice.dtos.requests.RequestUpdateProduct;
+import com.ibrahim.productservice.dtos.responses.*;
+import com.ibrahim.productservice.entities.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface ProductMapper {
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
+    Product requestAddProductToProduct(RequestAddProduct requestAddProduct);
+    ResponseAddProduct productToResponseAddProduct(Product product);
+
+    Product requestUpdateProductToProduct(RequestUpdateProduct requestUpdateProduct);
+    ResponseUpdateProduct productToResponseUpdateProduct(Product product);
+
+    Product requestGetProductByIdToProduct(RequestGetProductById requestGetProductById);
+    ResponseGetProductById productToResponseGetProductById(Product product);
+
+    Product requestGetProductByNameToProduct(RequestGetProductByName requestGetProductByName);
+    ResponseGetProductByName productToResponseGetProductByName(Product product);
+
+    List<ResponseGetAllProduct> productsToResponseGetAllProduct(List<Product> products);
+
+}
