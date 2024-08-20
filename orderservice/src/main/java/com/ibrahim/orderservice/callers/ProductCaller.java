a81f13570733f7bc2e57c8b303e7f0173440e728
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-08-20 13:00:44 +0300

    Order service logback configurations added. Tested on ProductCaller class.

--- orderservice/src/main/java/com/ibrahim/orderservice/callers/ProductCaller.java
+++ orderservice/src/main/java/com/ibrahim/orderservice/callers/ProductCaller.java
@@ -7,6 +7,8 @@ import com.ibrahim.common.dtos.productservice.responses.*;
 import com.ibrahim.orderservice.mapper.ProductMapper;
 import com.ibrahim.orderservice.webApi.clients.ProductServiceClient;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,12 +17,14 @@ public class ProductCaller implements ProductCallerService {
 
     private final ProductServiceClient productServiceClient;
     private final ProductMapper productMapper;
+    private static final Logger logger = LoggerFactory.getLogger(ProductCaller.class);
 
 
     @Override
     public ResponseGetProductFromProductService getProduct(RequestGetProductFromProductServiceById requestGetProductFromProductServiceById) {
         int id = requestGetProductFromProductServiceById.getId();
         ResponseGetProductById responseGetProductById = this.productServiceClient.getStockByProductId(id).getBody();
+        logger.info("Get product log: {}", ProductCaller.class.getSimpleName());
         return this.productMapper.responseGetProductByIdToResponseGetProductFromProductService(responseGetProductById);
     }
 
